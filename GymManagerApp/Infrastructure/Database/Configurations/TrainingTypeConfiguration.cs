76d9798ee6ae3a766252f305d41d8924a3ca8788
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2024-10-25 19:44:35 +0200

    feat: add database connection

---
+++ GymManagerApp/Infrastructure/Database/Configurations/TrainingTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using GymManagerApp.Domain.Entities.Training;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymManagerApp.Infrastructure.Database.Configurations
+{
+    public class TrainingTypeConfiguration : IEntityTypeConfiguration<TrainingType>
+    {
+        public void Configure(EntityTypeBuilder<TrainingType> builder)
+        {
+            builder.ToTable("TrainingType");
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+            builder.Property(t => t.Description).HasMaxLength(300);
+            builder.Property(t => t.Intensity).HasConversion<int>();
+        }
+    }
+}
---
+++ GymManagerApp/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using GymManagerApp.Domain.Entities.Training;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace GymManagerApp.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<TrainingType> Training { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
