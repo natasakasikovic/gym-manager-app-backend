9d113254dbf28a2b5fec72ae3819b39137a66a96
Natasa Kasikovic~ <nkasikovic@yahoo.com>
2025-03-13 00:58:31 +0100

    refactor: move service registrations and db configuration from Program.cs to dedicated classes for better SoC

---
+++ GymManagerApp/Presentation/Startup/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using GymManagerApp.Domain.RepositoryInterfaces;
+using GymManagerApp.Infrastructure.Database.Repositories;
+
+namespace GymManagerApp.Presentation.Startup
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			services.AddRepositories();
+			services.AddAutoMapper(typeof(Program).Assembly);
+			services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+			return services;
+		}
+
+		private static IServiceCollection AddRepositories(this IServiceCollection services)
+		{
+			services.AddScoped<ITrainingRepository, TrainingRepository>();
+			services.AddScoped<ITrainingTypeRepository, TrainingTypeRepository>();
+			services.AddScoped<ITrainerRepository, TrainerRepository>();
+			services.AddScoped<IAdminRepository, AdminRepository>();
+			services.AddScoped<IMemberRepository, MemberRepository>();
+			services.AddScoped<IMembershipRepository, MembershipRepository>();
+
+			return services;
+		}
+	}
+}
--- GymManagerApp/Program.cs
+++ GymManagerApp/Program.cs
@@ -1,38 +1,24 @@
-using GymManagerApp.Domain.RepositoryInterfaces;
-using GymManagerApp.Infrastructure;
-using GymManagerApp.Infrastructure.Database.Repositories;
-using Microsoft.EntityFrameworkCore;
+﻿using GymManagerApp.Presentation.Startup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-builder.Services.AddScoped<ITrainingRepository, TrainingRepository>();
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddApplicationServices();
+builder.Services.AddDatabaseConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
